8ff6d3e0c6057d12bb91f406ef14433d242e898e
yoohaso <solhanyun@gmail.com>
2025-09-26 17:11:27 +0900

    feat: add rotating & falling rectangles and regenerate when out of bounds

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,20 +1,65 @@
-import { useCallback, useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import Rectangle from './model/Rectangle';
+
+const RECTANGLE_SIZE = {
+  width: 50,
+  height: 50,
+};
+
+// NOTE: https://www.cemyuksel.com/cyCodeBase/soln/poisson_disk_sampling.html
+function poissonDisk1D(viewportWidth: number, rectWidth: number, margin: number) {
+  const minSpace = rectWidth + margin;
+  const maxPointCount = Math.floor(viewportWidth / minSpace);
+  const targetPointCount = Math.floor(Math.random() * (maxPointCount + 1));
+
+  const points: number[] = [];
+  const MAX_ATTEMPTS = 1000;
+  let attempts = 0;
+
+  while (points.length < targetPointCount && attempts < MAX_ATTEMPTS) {
+    attempts++;
+    const candidatePoint = Math.random() * (viewportWidth - rectWidth - margin * 2) + margin;
+    const validPoint = points.every(point => Math.abs(candidatePoint - point) >= minSpace);
+
+    if (validPoint) {
+      points.push(candidatePoint);
+      attempts = 0;
+    }
+  }
+
+  return points;
+}
+
+function generateBlueRect(viewportWidth: number) {
+  const xPoints = poissonDisk1D(viewportWidth, RECTANGLE_SIZE.width, 20);
+
+  if (xPoints.length < 2) {
+    return generateBlueRect(viewportWidth);
+  }
+
+  return xPoints.map(
+    xPoint => new Rectangle(xPoint, 0, RECTANGLE_SIZE.width, RECTANGLE_SIZE.height, 'blue'),
+  );
+}
 
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const rAFref = useRef<number | null>(null);
+  const blueRectRef = useRef(generateBlueRect(window.innerWidth));
 
-  const blueRectangle = useMemo(() => new Rectangle(100, 100, 100, 100, 'blue'), []);
+  const drawRectangle = useCallback((ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    blueRectRef.current.forEach(rec => {
+      rec.animate(ctx, 0.1, 2);
+    });
 
-  const drawRectangle = useCallback(
-    (ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      blueRectangle.animate(ctx, 0.1, 2);
-      window.requestAnimationFrame(() => drawRectangle(ctx, canvas));
-    },
-    [blueRectangle],
-  );
+    const anyOutOfBounds = blueRectRef.current.some(rect => rect.y >= canvas.height);
+    if (anyOutOfBounds) {
+      blueRectRef.current = generateBlueRect(canvas.width);
+    }
+
+    window.requestAnimationFrame(() => drawRectangle(ctx, canvas));
+  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
